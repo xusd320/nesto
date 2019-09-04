925c520487edfa8ea001ec406ea23efa2b85ef8c
xushaodong <xushaodong@guazi.com>
2019-09-04 20:38:52 +0800

    update test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -16,8 +16,8 @@ describe('AppController (e2e)', () => {
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/healthCheck')
       .expect(200)
-      .expect('Hello World!');
+      .expect('ok');
   });
 });
