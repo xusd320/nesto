e0e0eaaeaf1ee7e4f39d38a1ef6d1ddadd6c314b
xushaodong <xushaodong@guazi.com>
2019-09-06 22:15:05 +0800

    update env

--- src/module/logger.module.ts
+++ src/module/logger.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { WinstonModule } from 'nest-winston';
 import * as winston from 'winston';
 import { ConfigService } from 'nestjs-config';
-import * as DailyRotateFile from 'winston-daily-rotate-file';
+// import * as DailyRotateFile from 'winston-daily-rotate-file';
 
 @Module({
   imports: [
@@ -14,19 +14,19 @@ import * as DailyRotateFile from 'winston-daily-rotate-file';
             i => `${i.timestamp} ${i.level.toUpperCase()} ${config.get('conf.appName')}  - - ${i.message}`,
           ),
         ),
-        transports: [
-          ...(
-            config.has('conf.logger.file') ? [
-              new DailyRotateFile({
-               level: config.get('conf.logger.level'),
-               filename: `${config.get('conf.appName').replace(/\//, '_')}${config.get('conf.logger.file')}`,
-               dirname: config.get('conf.logger.dir'),
-               datePattern: 'YYYY-MM-DD',
-               handleExceptions: true,
-              } as any),
-            ] : []
-          ),
-        ],
+        // transports: [
+        //   ...(
+        //     config.has('conf.logger.file') ? [
+        //       new DailyRotateFile({
+        //        level: config.get('conf.logger.level'),
+        //        filename: `${config.get('conf.appName').replace(/\//, '_')}${config.get('conf.logger.file')}`,
+        //        dirname: config.get('conf.logger.dir'),
+        //        datePattern: 'YYYY-MM-DD',
+        //        handleExceptions: true,
+        //       } as any),
+        //     ] : []
+        //   ),
+        // ],
         exitOnError: false, // do not exit on handled exceptions
       }),
       inject: [ConfigService],
