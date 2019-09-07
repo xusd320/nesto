c607f79142ab94e0cc748af51500c1d53035525d
xushaodong <xushaodong@guazi.com>
2019-09-07 12:07:20 +0800

    update config

--- src/module/config.module.ts
+++ src/module/config.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule, ConfigService } from 'nestjs-config';
 
 @Module({
   imports: [
-    ConfigModule.resolveRootPath(__dirname).load(`config/*.${process.env.NODE_ENV}.{ts,js}`, {
+    ConfigModule.resolveRootPath(__dirname).load(`../config/*.${process.env.NODE_ENV}.{ts,js}`, {
       modifyConfigName: name => name.replace(`.${process.env.NODE_ENV}`, ''),
     }),
  ],
