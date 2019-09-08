550837edbdb14c4691f80b11c7de04f89b6c071c
xushaodong <xushaodong@guazi.com>
2019-09-08 10:16:11 +0800

    update logger

--- src/module/logger.module.ts
+++ src/module/logger.module.ts
@@ -3,6 +3,7 @@ import { WinstonModule } from 'nest-winston';
 import * as winston from 'winston';
 import { ConfigService } from 'nestjs-config';
 import * as DailyRotateFile from 'winston-daily-rotate-file';
+import * as moment from 'moment';
 
 @Module({
   imports: [
@@ -11,21 +12,28 @@ import * as DailyRotateFile from 'winston-daily-rotate-file';
         format: winston.format.combine(
           winston.format.timestamp(),
           winston.format.printf(
-            i => `${i.timestamp} ${i.level.toUpperCase()} ${config.get('conf.appName')}  - - ${i.message}`,
+            i =>
+              `${moment(i.timestamp).format(
+                'YYYY-MM-DDTHH:mm:ss.SSSZZ',
+              )} ${i.level.toUpperCase()} ${config.get('conf.appName')}  - - ${
+                i.message
+              }`,
           ),
         ),
         transports: [
-          ...(
-            config.has('conf.logger.file') ? [
-              new DailyRotateFile({
-               level: config.get('conf.logger.level'),
-               filename: `${config.get('conf.appName').replace(/\//, '_')}${config.get('conf.logger.file')}`,
-               dirname: config.get('conf.logger.dir'),
-               datePattern: 'YYYY-MM-DD',
-               handleExceptions: true,
-              } as any),
-            ] : []
-          ),
+          ...(config.has('conf.logger.file')
+            ? [
+                new DailyRotateFile({
+                  level: config.get('conf.logger.level'),
+                  filename: `${config
+                    .get('conf.appName')
+                    .replace(/\//, '_')}${config.get('conf.logger.file')}`,
+                  dirname: config.get('conf.logger.dir'),
+                  datePattern: 'YYYY-MM-DD',
+                  handleExceptions: true,
+                } as any),
+              ]
+            : []),
           new winston.transports.Console({
             level: config.get('conf.logger.level'),
             handleExceptions: true,
