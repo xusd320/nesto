57352b9ab8ef5e7f2201d87b0b4c5341740d4f01
xushaodong <xushaodong@guazi.com>
2019-11-01 15:59:14 +0800

    update zk

--- src/config/conf.online.ts
+++ src/config/conf.online.ts
@@ -1,4 +1,5 @@
 import defaultConf from './conf.default';
+import { Zk } from 'dubbo2.js';
 
 export default  {
   ...defaultConf,
@@ -13,9 +14,12 @@ export default  {
   dubbo: {
     options : {
       application: { name: 'nest-dubbo' },
-      register: 'zk01.dns.guazi.com:2181,zk02.dns.guazi.com:2181,zk03.dns.guazi.com:2181',
-      zkAuthInfo: { scheme: 'digest', auth: 'xinchezk:nhLuJ0RlGUU' },
-      zkRoot: 'xinche',
+      register: Zk({
+        url: 'zk01.dns.guazi.com:2181,zk02.dns.guazi.com:2181,zk03.dns.guazi.com:2181',
+        zkAuthInfo: { scheme: 'digest', auth: 'xinchezk:nhLuJ0RlGUU' },
+        zkRoot: 'xinche',
+      }),
+
     },
     matches: [
       {
